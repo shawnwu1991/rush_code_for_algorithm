09a6bdc3cdfbdcf51e906b244d8f863f25909a62
shawnwu1991 <shawnwu1991@gmail.com>
2015-05-27 00:12:54 +0800

    Algorithm(Easy) Happy Number.

--- Happy Number/Happy_Number_sol_2.cpp
+++ Happy Number/Happy_Number_sol_2.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+// using hash table to check the end of a loop
+// noting that unordered_map replacing the hash_map in C++ 11.
+
 class Solution {
     public:
     bool isHappy(int n)
