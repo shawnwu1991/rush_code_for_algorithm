e2ac0fa7023300f26feec393eddc5898469bab22
shawnwu1991 <shawnwu1991@gmail.com>
2015-05-26 23:59:37 +0800

    Algorithm(Easy) Add Binary.

--- Add Binary/Add_Binary.cpp
+++ Add Binary/Add_Binary.cpp
@@ -23,7 +23,7 @@ class Solution {
 	else
 	    iterNum = bNum;
 	
-	while (iterNum > 0) {
+	while (iterNum >= 0) {
 	    if ((a.at(aNum) == b.at(bNum)) && (0 == a.at(aNum))) {
 	    	tempChar = '0';
 		if (true == isCarry) {
@@ -44,12 +44,53 @@ class Solution {
 		    tempChar = '1';
 	    }
 	    reverseResult.push_back(temp);
+	    iterNum--;
+	    aNum--;
+	    bNum--;
+	}
+
+	string tempResult;
+	iterNum = reverseResult.size();
+	while (iterNum >= 0) {
+	    tempResult.push_back(reverseResult.at(iterNum));
+	    iterNum--;
 	}
 
 	if (true == isCarry) {
-	    
+	    int i = 0;
+	    if (a.size() <= b.size()) {
+	    	iterNum = b.size() - a.size();
+		if (b.size(0) == '1')
+		    result.push_back('1');
+		
+		while (i < iterNum) {
+		    if (b.at(i) == 0)
+		    	result.push_back('1');
+		    else
+		    	result.push_back('0');
+		}
+	    }else {
+	    	iterNum = a.size() - b.size();
+		if (a.size(0) == '1')
+		    result.push_back('1');
+		
+		while (i < iterNum) {
+		    if (a.at(i) == 0)
+		    	result.push_back('1');
+		    else
+		    	result.push_back('0');
+		}
+	    }
 	}else {
-	    
+	    if (a.size() <= b.size()) {
+	   	iterNum = b.size() - a.size();
+		result = b.substr(0, iterNum);
+	    }else {
+	    	iterNum = a.size() - b.size();
+		result = a.substr(0, iterNum);
+	    }
 	}
+	
+	return (result += tempResult);
     }
 };
