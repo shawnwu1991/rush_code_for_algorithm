159422cb3522b47b50bb788062dbe4fcc0b934f2
shawnwu1991 <shawnwu1991@gmail.com>
2015-06-01 12:28:02 +0800

    Algorithm(Easy) Contains Duplicate II.

--- Contains Duplicate II/Contains_Duplicate.cpp
+++ Contains Duplicate II/Contains_Duplicate.cpp
@@ -5,6 +5,31 @@ using namespace std;
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-    	
+    	if (2 > nums.size()) return false;
+
+	bool result = false;
+	for (size_t i = 0; i < nums.size() ; ++i) {
+	    result = isNearbyDuplicate(nums, i, k);
+	    if (result) return true;
+	}
+    	return result;
+    }
+
+    bool isNearbyDuplicate(vector<int>& nums, size_t i, int k) {
+    	vector<int> temp;
+	vector<int>::iterator it;
+	temp.push_back(nums.at(i));
+	nums.erase(i);
+	
+	it = find_end(nums.begin(), nums.end(), temp.begin(), temp.end());
+	if (it != nums.end()) {
+	    nums.erase(it);
+	    it = find(nums.begin(), nums.end(), temp.at(0));
+	    if (it != nums.end()) {
+	    	
+	    }else 
+	    	return true;
+	}else 
+	    return false;
     }
 };
