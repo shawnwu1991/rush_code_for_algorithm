66b132daeec10f3b8d39e26caef129a24579e10c
shawnwu1991 <shawnwu1991@gmail.com>
2015-06-02 17:33:06 +0800

    Algorithm(Easy) Count Primes.

--- Count Primes/Count_Primes.cpp
+++ Count Primes/Count_Primes.cpp
@@ -5,7 +5,19 @@ using namespace std;
 class Solution {
 public:
     int countPrimes(int n) {
-    	int result = 0; 
-    	return ;
+    	 bool *temp = new bool[n];
+	 memset(temp, true, sizeof(bool) * n);
+	 for (size_t i = 2; pow(n, 2) < n; ++i)
+	     if (temp[i])
+	     	for (size_t j = 2; i * j < n; ++j)
+		    temp[i * j] = false;
+	 
+	 int result = 0;
+	 for (size_t i = 2; i < n; ++i)
+	     if (temp[i])
+	     	++result;
+
+	 delete[] temp;
+	 return result;
     }
 };
