177a365ade72d58f7f516527a407ec51f3ed2804
shawnwu1991 <shawnwu1991@gmail.com>
2015-06-02 11:35:49 +0800

    Algorithm(Easy) Compare Version Numbers.

--- Compare Version Numbers/Compare_Version_Numbers.cpp
+++ Compare Version Numbers/Compare_Version_Numbers.cpp
@@ -5,7 +5,16 @@ using namespace std;
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-   	int result;
-	return result;
+    	if ((!version1) && (!version2)) return 0;
+	if (!version1) version1 = "0.";
+	if (!version2) version2 = "0.";
+	if (string::npos == version1.find('.')) version1.push_back('.');
+	if (string::npos == version2.find('.')) version2.push_back('.');
+
+   	int result = stoi(version1.substr(0, version1.find('.'))) - 
+		     stoi(version2.substr(0, version2.find('.')));
+	if (result > 0) return -1;
+	else if (result < 0) return 1;
+	else return compareVersion(version1.substr(version1.find('.'), version1.end()), version2.substr(version2.find('.'), version2.end()));
     }
 };
