489463f7fd5043c85d9dc900f382d1e4d80f041f
shawnwu1991 <shawnwu1991@gmail.com>
2015-06-02 11:29:06 +0800

    Algorithm(Easy) Count and Say.

--- Count and Say/Count_and_Say.cpp
+++ Count and Say/Count_and_Say.cpp
@@ -5,14 +5,22 @@ using namespace std;
 class Solution {
 public:
     string countAndSay(int n) {
-    	string result = itoa(n);
+    	string result = to_string(n);
     	for (size_t i = 0; i < n; ++i) 
 	    result = countDealing(result);
 	return result;
     }
 
     string countDealing(string result) {
-    	
-	return ;
+    	string tempResult;
+	int counter = 1;
+    	for (size_t i = result.size() - 1; i >= 0; --i) {
+	    if (counter > 1) {
+	    	if (i >)
+	    }
+	    tempResult.insert(0, result[i]);
+	    tempResult.insert(0, to_string(counter));
+	}
+	return tempResult;
     }
 };
