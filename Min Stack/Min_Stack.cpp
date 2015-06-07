7cc514a405990b932345780e3b0291d25dd753e0
Shawn Wu <shawnwu1991@gmail.com>
2015-06-07 21:02:36 +0800

    Algorithm(Easy) Min Stack.

--- Min Stack/Min_Stack.cpp
+++ Min Stack/Min_Stack.cpp
@@ -2,21 +2,28 @@
 
 using namespace std;
 
-class Solution {
+class MinStack {
 public:
     void push(int x) {
-
+        minStack.push_back(x);
     }
 
     void pop() {
-
+        minStack.pop_back();
     }
 
     int top() {
-
+        return minStack.back();
     }
 
     int getMin() {
-
+        int result = minStack.at(0);
+        for (size_t i = 0; i < mimStack.size(); ++i) {
+             if (result > minStack[i]) result = minStack[i];
+        }
+        return result;
     }
+
+private:
+    vector<int> minStack;
 };
