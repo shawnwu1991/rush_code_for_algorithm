ca79d2ba744e0c708f587b4cc491a0ce9fbf8b3c
Shawn Wu <shawnwu1991@gmail.com>
2015-06-07 17:30:16 +0800

    Algorithm(Easy) Rotate Array.

--- Rotate Array/Rotate_Array_2.cpp
+++ Rotate Array/Rotate_Array_2.cpp
@@ -5,9 +5,12 @@ using namespace std;
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        if (0 == nums.size() || 1 == nums.size()) return ;
+        int temp;
         reverse(nums.begin(), nums.end());
         for (size_t i = 0; i < k; ++i) {
-            nums.push_back(nums.begin());
+            temp = nums.begin();
+            nums.push_back(temp;
             nums.erase(nums.begin());
         }
         reverse(nums.begin(), nums.end());
