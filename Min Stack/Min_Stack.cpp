1a0adcace7d36433525033cb70444fdb0b545223
Shawn Wu <shawnwu1991@gmail.com>
2015-06-07 17:42:04 +0800

    Algprithm(Easy) Min Stack.

---
+++ Min Stack/Min_Stack.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    void push(int x) {
+
+    }
+
+    void pop() {
+
+    }
+
+    int top() {
+
+    }
+
+    int getMin() {
+
+    }
+};
