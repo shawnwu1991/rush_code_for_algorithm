7a0bdc070a9effbd041419f88917ce2b644b1e2c
Shawn Wu <shawnwu1991@gmail.com>
2015-06-10 16:56:41 +0800

    Algorithm(Medium) Basic Calculator.

--- Basic Calculator/Basic_Calculator.cpp
+++ Basic Calculator/Basic_Calculator.cpp
@@ -5,7 +5,34 @@ using namespace std;
 class Solution {
 public:
     int calculate(string s) {
-        int result;
+        stack<int> calculator;
+        int temp;
+        for (size_t i = 0; i < s.size(); ++i) {
+            if (' ' == s[i]) continue;
+
+            if ('0' <= s[i] && s[i] <= '9') {
+                calculator.push_back((s[i] - '0'));
+                continue;
+            }
+
+            if () {
+
+            }
+
+
+
         return result;
+    }
+
+    stack<int> getFormulation(string s) {
+        stack<int> tempForm;
+        for (size_t i = 0; i < s.size(); ++i) {
+            if ('' == s[i]) continue;
+
+            if ('+' == s[i] || '-' == s[i]) {
+
+            }else
+                tempForm.push_back(s[i]);
+        }
     }
 };
