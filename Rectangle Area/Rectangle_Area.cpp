e694d872b7be6f14f3508ef70e5e29b789dd0c0a
Shawn Wu <shawnwu1991@gmail.com>
2015-06-10 23:53:31 +0800

    Algorithm(Easy) Rectangle Area.

--- Rectangle Area/Rectangle_Area.cpp
+++ Rectangle Area/Rectangle_Area.cpp
@@ -5,7 +5,9 @@ using namespace std;
 class Solution {
 public:
     int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        int result = 0;
-        return ;
+        int overlap_width = max((min(C, G) - max(A, E)), 0);
+        int overlap_height = max((min(D, H) - max(B, F)) , 0);
+
+        return ((C - A) * (D - B) + (G - E) * (H - F) - overlap_width * overlap_height);
     }
 };
