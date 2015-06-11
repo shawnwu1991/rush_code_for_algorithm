7c820db629254a7ca31eaae16a34427934c594ee
Shawn Wu <shawnwu1991@gmail.com>
2015-06-12 00:02:02 +0800

    Algorithm(Medium) Count Complete Tree Nodes.

--- Count Complete Tree Nodes/Count_Complete_Tree_Nodes.cpp
+++ Count Complete Tree Nodes/Count_Complete_Tree_Nodes.cpp
@@ -8,12 +8,24 @@ using namespace std;
  *      int val;
  *      TreeNode* left;
  *      TreeNode* right;
- *      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *      TreeNode(int: x) : val(x), left(NULL), right(NULL) {}
  * };*/
 
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        return ;
+        if (NULL == root) return 0;
+        TreeNode* tempRoot = root;
+        int level = 0;
+        while (true) {
+            if (tempRoot->left) {
+                tempRoot = tempRoot->right;
+                if (tempRoot) {
+                    level += 1;
+                    continue;
+                }
+            return ;
+            }
+        }
     }
 };
