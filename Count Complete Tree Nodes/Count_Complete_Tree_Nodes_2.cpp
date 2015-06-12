3dcda3f0f36b11d0403e9f0ed5b6173b11bd4cf5
Shawn Wu <shawnwu1991@gmail.com>
2015-06-12 17:08:50 +0800

    Algorithm(Medium) Count Complete Tree Nodes(Better Answer).

---
+++ Count Complete Tree Nodes/Count_Complete_Tree_Nodes_2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *      int val;
+ *      TreeNode* left;
+ *      TreeNode* right;
+ *      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };*/
+
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (!root) return 0;
+         int leftHeight = 0, rightHeight = 0;
+         TreeNode* leftRoot = root;
+         TreeNode* rightRoot = root;
+
+         while (leftRoot) {
+              leftHeight += 1;
+              leftRoot = leftRoot->left;
+         }
+
+         while (rightRoot) {
+              rightRoot += 1;
+              rightRoot = rightRoot->right;
+         }
+
+         if (leftRoot == rightRoot) return (pow(2, leftRoot) - 1);
+
+         return (1 + countNodes(root->left) + countNodes(root->right));
+    }
+};
