e1d68361320bd2a8f229a39a939ae5e909f6db59
Shawn Wu <shawnwu1991@gmail.com>
2015-06-13 23:01:22 +0800

    Algorithm(Medium) Implement Stack using Queues.

---
+++ Implement Stack using Queues/Implement_Stack_using_Queues.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class Stack {
+public:
+    // Push element x onto stack.
+    void push(int x) {
+
+    }
+
+    // Removes the element on top of the stack.
+    void pop() {
+
+    }
+
+    // Get the top element.
+    int top() {
+
+    }
+
+    // Return whether the stack is empty.
+    bool empty() {
+
+    }
+};
