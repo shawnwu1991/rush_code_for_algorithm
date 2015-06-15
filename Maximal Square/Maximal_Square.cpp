8b6e207e45fed1c10123c9084e97c6903595c8c2
Shawn Wu <shawnwu1991@gmail.com>
2015-06-15 23:04:58 +0800

    Algorithm(Medium) Maximal Square.

--- Maximal Square/Maximal_Square.cpp
+++ Maximal Square/Maximal_Square.cpp
@@ -5,6 +5,8 @@ using namespace std;
 class SOlution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size();
+        int n = matrix.size();
         return ;
     }
 };
