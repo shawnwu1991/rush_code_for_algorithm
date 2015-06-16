02bb8d328298ec76e65365ea420273458ffbf25b
Shawn Wu <shawnwu1991@gmail.com>
2015-06-16 14:17:06 +0800

    Algorithm(Easy) Minimum Depth of Binary Tree(Shortest Answer).

--- Minimum Depth of Binary Tree/Minimum_Depth_of_Binary_Tree_2.cpp
+++ Minimum Depth of Binary Tree/Minimum_Depth_of_Binary_Tree_2.cpp
@@ -7,8 +7,8 @@ public:
     int minDepth(TreeNode* root) {
         if (!root) return 0;
         if (root->left && root->right)
-            return (min(minDepth(root->left), minDepth(root->right)));
+            return (min(minDepth(root->left), minDepth(root->right)) + 1);
         else
-            return (max(minDepth(root->left), minDepth(root->right)));
+            return (max(minDepth(root->left), minDepth(root->right)) + 1);
     }
 };
