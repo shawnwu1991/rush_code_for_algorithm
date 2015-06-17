2765b3a2155a715ea002cd4be7f32916570c6b68
Shawn Wu <shawnwu1991@gmail.com>
2015-06-17 14:54:29 +0800

    Algorithm(Easy) Isomorphic Strings.

--- Isomorphis Strings/Isomorphic_Strings.cpp
+++ Isomorphis Strings/Isomorphic_Strings.cpp
@@ -5,6 +5,23 @@ using namespace std;
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-    	
+    	if (!s.length()) return true;
+
+        int len = s.length();
+        char sMap[256];
+        char tMap[256];
+        memset(sMap, 0, (sizeof(char) * 256));
+        memset(tMap, 0, (sizeof(char) * 256));
+
+        for (int i = 0; i < len; ++i) {
+             if (!sMap[s[i]]) sMap[s[i]] = t[i];
+             else if (sMap[s[i]] != t[i]) return false;
+             else ;
+
+             if (!tMap[t[i]]) tMap[t[i]] = s[i];
+             else if (tMap[t[i]] != s[i]) return false;
+             else ;
+        }
+        return true;
     }
 };
