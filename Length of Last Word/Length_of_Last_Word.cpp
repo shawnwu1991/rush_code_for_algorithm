7cdd6179417806d308a0f781bf613618ead58219
Shawn Wu <shawnwu1991@gmail.com>
2015-06-17 23:58:40 +0800

    Algorithm(Easy) Length of Last Word.

--- Length of Last Word/Length_of_Last_Word.cpp
+++ Length of Last Word/Length_of_Last_Word.cpp
@@ -5,6 +5,21 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        return ;
+        if (!s.length()) return 0;
+
+        int bLen = 0;
+        int lLag = 0;
+        reverse(s.begin(), s.end());
+        string::iterator iter = s.begin();
+        while (iter != s.end()) {
+            if ((*iter != ' ') && (bLen <= 1)) {
+                 bLen++;
+                 llen++;
+            }
+            else if ((*iter == ' ') && (bLen <= 1)) iter++;
+            else if ((*iter == ' ') && (bLen == 1)) return lLen;
+            else ;
+        }
+        return 0;
     }
 };
