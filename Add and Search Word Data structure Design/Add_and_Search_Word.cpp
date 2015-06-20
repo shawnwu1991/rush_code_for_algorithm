a96a4f4abf771c31ca970ffa6aead857fbb1f00c
Shawn Wu <shawnwu1991@gmail.com>
2015-06-20 23:40:30 +0800

    Algorithm(Medium) Add and Search Word Data.

---
+++ Add and Search Word Data structure Design/Add_and_Search_Word.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class WordDictionary {
+public:
+
+    //Adds a word into the data structure.
+    void addWord(string word) {
+
+    }
+
+    //Returns if the word is in the data structure. A word could contain
+    //the dot character '.' to represent any one letter.
+    bool search(string word) {
+         return ;
+    }
+};
+
+//Your WordDictionary object will be instantiated and called as such:
+//WordDictionary wordDictionary;
+//wordDictionary.addWord("word");
+//wordDictionary.search("pattern");
