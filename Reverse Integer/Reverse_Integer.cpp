c43dd7825f16ca17824863325f04831ddda7d2d4
Shawn Wu <shawnwu1991@gmail.com>
2015-06-21 23:55:42 +0800

    Algorithm(Easy) Reverse Integer.

--- Reverse Integer/Reverse_Integer.cpp
+++ Reverse Integer/Reverse_Integer.cpp
@@ -5,6 +5,18 @@ using namespace std;
 class Solution {
 public:
     int reverse(int x) {
-        return ;
+        string result;
+        bool flag = false;
+        int temp;
+        if (x < 0) {
+            flag = true;
+            x = -x;
+        }
+        while (x >= 0) {
+            temp = x % 10;
+            if (!temp) continue;
+            result.push_back(temp - MARGIN);
+        }
+        return flag ? -atoi(result) : atoi(result);
     }
 };
