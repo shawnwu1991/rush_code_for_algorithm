65c8e6485317425e3876e81aa4f98a2fd3b71130
Shawn Wu <shawnwu1991@gmail.com>
2015-06-22 23:57:38 +0800

    Algorithm(Easy) Zigzag Conversion.

--- Zigzag Conversion/Zigzag_Conversion.cpp
+++ Zigzag Conversion/Zigzag_Conversion.cpp
@@ -5,6 +5,11 @@ using namespace std;
 class Solution {
 public:
     string convert(string s, int numRows) {
+        if (numRows >= s.length() || numRows <= 1) return s;
+        string result;
+        for (int i = 0; i < numRows; ++i) {
+
+        }
         return ;
     }
 };
