813a8052a0b6f9c46d86966a198e2adb3bf6f26b
Shawn Wu <shawnwu1991@gmail.com>
2015-06-24 11:28:32 +0800

    Algorithm(Easy) Zigzag Conversion.

--- Zigzag Conversion/Zigzag_Conversion.cpp
+++ Zigzag Conversion/Zigzag_Conversion.cpp
@@ -7,9 +7,24 @@ public:
     string convert(string s, int numRows) {
         if (numRows >= s.length() || numRows <= 1) return s;
         string result;
-        for (int i = 0; i < numRows; ++i) {
-
+        for (int i = 1; i <= numRows; ++i) {
+            result += convertOneRow(s, i, numRows);
         }
-        return ;
+        return result;
+    }
+
+    string convertOneRow(string s, int row, int numRows) {
+        string result;
+        int j = 0;
+        if (1 == row || numRows == row)
+            for (int i = row - 1 + (2 * numRows - 2) * j; i < s.length(); ++j)
+                result.push_back(s[i]);
+        else {
+             int temp = 0;
+             for (int i = row - 1 + (2 * numRows - 2) * j + temp; i < s.length(); ++j) {
+                 result.push_back(s[i]);
+                 temp = 2 * numRows - 2 - temp;
+             }
+        }
     }
 };
