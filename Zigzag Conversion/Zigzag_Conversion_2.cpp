4df80943ca31e214ba6fdfbd71a5f696c5fc20b2
Shawn Wu <shawnwu1991@gmail.com>
2015-06-24 11:38:00 +0800

    Algorithm(Easy) Zigzag Conversion.

---
+++ Zigzag Conversion/Zigzag_Conversion_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (1 == numRows || (!s.length())) return s;
+        string result;
+        int offsetLen = 2 * numRows - 2;
+
+        for (int i = 0; i < numRows; ++i)
+            if ((!i) || (numRows - 1) == i)
+                for (int j = i; j < s.length(); j += offsetLen) result.push_back(s[j]);
+            else {
+                 int temp = 2 * i;
+                 for (int j = i; j < s.length(); temp = offsetLen - temp, j += temp) result.push_back(s[j]);
+            }
+        return result;
+    }
+};
