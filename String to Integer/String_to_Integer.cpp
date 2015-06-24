428f79b457ed65cbafaa4bfd6ac109a38afc71de
Shawn Wu <shawnwu1991@gmail.com>
2015-06-24 15:36:26 +0800

    Algorithm(Easy) String to Integer.

--- String to Integer/String_to_Integer.cpp
+++ String to Integer/String_to_Integer.cpp
@@ -5,6 +5,39 @@ using namespace std;
 class Solution {
 public:
     int myAtoi(string str) {
-        return ;
+        if (!str.length()) return 0;
+        int result = 0;
+        bool nonNeg = true;
+        for (int i = 0; i < str.length(); ++i) {
+            switch (str[i]) {
+                case '-':
+                    nonNeg = false; break;
+                case ' ':
+                    break;
+                case '0':
+                    result *= 10; break;
+                case '1':
+                    result = result * 10 + 1; break;
+                case '2':
+                    result = result * 10 + 2; break;
+                case '3':
+                    result = result * 10 + 3; break;
+                case '4':
+                    result = result * 10 + 4; break;
+                case '5':
+                    result = result * 10 + 5; break;
+                case '6':
+                    result = result * 10 + 6; break;
+                case '7':
+                    result = result * 10 + 7; break;
+                case '8':
+                    result = result * 10 + 8; break;
+                case '9':
+                    result = result * 10 + 9; break;
+                default: break;
+            }
+
+        }
+        return result;
     }
 };
