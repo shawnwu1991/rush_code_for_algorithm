7ecfc406c2d035307b986455f764ec7d379d112c
Shawn Wu <shawnwu1991@gmail.com>
2015-06-25 10:29:18 +0800

    Algorithm(Easy) Palindrome Number.

--- Palindrome Number/Palindrome_Number.cpp
+++ Palindrome Number/Palindrome_Number.cpp
@@ -5,6 +5,23 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(int x) {
-        return ;
+        if (0 == x) return true;
+        x = abs(x);
+        int len = lenInt(x);
+        while (x != 0 && len != 1) {
+            if ((x % 10) != (x / pow(10, len))) return false;
+            x = x - (x / pow(10, len)) * pow(10, len);
+            x /= 10;
+            len--;
+        }
+        return true;
+    }
+    int lenInt(int x) {
+         int len = 0;
+         while (x != 0) {
+              len++;
+              x /= 10;
+         }
+         return len;
     }
 };
