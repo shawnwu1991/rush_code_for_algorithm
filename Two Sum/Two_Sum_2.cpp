ee9f6c27da588ba86de306a6bef0143bda757688
Shawn Wu <shawnwu1991@gmail.com>
2015-06-27 00:09:30 +0800

    Algorithm(Medium) Two Sum (Better Answer).

--- Two Sum/Two_Sum_2.cpp
+++ Two Sum/Two_Sum_2.cpp
@@ -12,7 +12,8 @@ public:
             mapping[nums[i]] = i;
         for (int i = 0; i < nums.length(); ++i) {
             int leaveNum = target - nums[i];
-            if (mapping.find(leaveNum) != nums.end()) {
+            if ((mapping.find(leaveNum) != nums.end()) &&
+                    mapping[leaveNum] != i) {
                  result.push_back(i + 1);
                  result.push_back(mapping[leaveNum] + 1);
                  return result;
