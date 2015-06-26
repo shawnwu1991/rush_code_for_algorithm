8bc73e0eca423dbde6ea2b7df3315a903d89b72b
Shawn Wu <shawnwu1991@gmail.com>
2015-06-26 17:13:44 +0800

    Algorithm(Medium) Two Sum.

--- Two Sum/Two_Sum.cpp
+++ Two Sum/Two_Sum.cpp
@@ -9,7 +9,7 @@ public:
         for (int i = 0; i < nums.length(); ++i)
             for (int j = i + 1; i < nums.length(); ++j)
                 if (nums[j] == (target - nums[i]))
-                    return result.push_back(i, j);
+                    return result.push_back(i + 1, j + 1);
         return result;
     }
 };
