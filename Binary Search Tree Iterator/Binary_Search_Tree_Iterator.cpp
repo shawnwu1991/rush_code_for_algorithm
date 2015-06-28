8d0cbeaf32fa51e0205bf30cc4fbc05e183551e9
Shawn Wu <shawnwu1991@gmail.com>
2015-06-28 08:27:26 +0800

    Algorithm(Medium) Binary Search Tree Iterator.

---
+++ Binary Search Tree Iterator/Binary_Search_Tree_Iterator.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+/*Definition for binary tree
+ * struct TreeNode {
+ *      int val;
+ *      TreeNode *left;
+ *      TreeNode *right;
+ *      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };*/
+
+class BSTIterator {
+public:
+    BSTIterator(TreeNode *root) {
+
+    }
+
+    /** @return whether we have a next smallest number. */
+    bool hasNext() {
+         return ;
+    }
+
+    int next() {
+         return ;
+    }
+
+    /**
+     * Your BSTIterator will be called like this:
+     * BSTIterator i = BSTIterator(root);
+     * while (i.hasNext()) cout << t.next();
+     */
+};
