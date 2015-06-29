c222f6aa870869336761419be650de2fdd6fb3d3
Shawn Wu <shawnwu1991@gmail.com>
2015-06-29 23:23:11 +0800

    Algorithm(Medium) Longest Substring Without Repeating Characters.

--- Longest Substring Without Repeating Characters/Longest_Substring_Without_Repeating_Characters.cpp
+++ Longest Substring Without Repeating Characters/Longest_Substring_Without_Repeating_Characters.cpp
@@ -5,6 +5,15 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        return ;
+        int len = 0, result = 0;
+        unordered_map<char, int> hashT;
+        for (int i = 0; i < 26; ++i)
+            hashT['a' + i] = 0;
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (!hashT[s[i]]) {len++; continue; }
+            else result = max(len, result);
+        }
+        return result;
     }
 };
