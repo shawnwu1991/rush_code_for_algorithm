3b461f13fb49d3ee95849c27531610de31578025
Shawn Wu <shawnwu1991@gmail.com>
2015-06-29 22:03:52 +0800

    Algorithm(Medium) Add Two Numbers (Better Answer).

---
+++ Add Two Numbers/Add_Two_Numbers_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        return addTwoLists(l1, l2, 0);
+    }
+
+    ListNode* addTwoLists(ListNode* l1, ListNode* l2, int carry) {
+        if (!l1 && !l2)
+            if (carry) return new ListNode(carry);
+            else return NULL;
+
+        int val = carry + (l1? l1->val: 0) + (l2? l2->val: 0);
+        ListNode* currentNode = new ListNode(val % 10);
+        currentNode->next = addList(l1? l1->val: NULL, l2? l2->val: NULL, val / 10);
+        return currentNode;
+    }
+};
