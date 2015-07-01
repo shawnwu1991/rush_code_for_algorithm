32b68e0e81812cdc1ae1c3ef0a7796f3b4b0c165
Shawn Wu <shawnwu1991@gmail.com>
2015-07-01 23:39:52 +0800

    Algorithm(Medium) Two Sum II(Better Answer!).

--- Two Sum II/Two_Sum_II.cpp
+++ Two Sum II/Two_Sum_II.cpp
@@ -5,6 +5,22 @@ using namespace std;
 class TwoSumII {
 public:
     int[] twoSum(int[] numbers, int target) {
-        return ;
+        int[] result = {-1, -1};
+
+        if (!numbers || !numbers.length) return result;
+
+        int start = 0;
+        int end = numbers.length - 1;
+
+        while (start < end) {
+             int sum = numbers[start] + numbers[end];
+             if (sum == target) {
+                 result[0] = start + 1;
+                 result[1] = end + 1;
+                 break;
+             }else if (sum < target) start++;
+             else end--;
+        }
+        return result;
     }
 };
