ac0681b4f49a579a3bd9c98447358e00ce871034
Shawn Wu <shawnwu1991@gmail.com>
2015-07-03 00:03:26 +0800

    Algorithm(Medium) Swap Nodes in Pairs(Better Answer!).

---
+++ Swap Nodes in Pairs/Swap_Nodes_in_Pairs_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *      int val;
+ *      ListNode *left;
+ *      ListNode *right;
+ *      ListNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };*/
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+         if (!head || !head->next) return head;
+         ListNode *tempHead = head, *newHead = head->next;
+         newHead->next = tempHead;
+         tempHead->next = swapPairs(head->next->next);
+         return newHead;
+    }
+};
