e2fc8b374adba631fa456a5d7d4cc7ff433e55b1
Shawn Wu <shawnwu1991@gmail.com>
2015-07-03 23:09:23 +0800

    Algorithm(Easy) Roman to Integer.

--- Roman to Integer/Roman_to_Integer.cpp
+++ Roman to Integer/Roman_to_Integer.cpp
@@ -5,7 +5,27 @@ using namespace std;
 class Solution {
 public:
     int romanToInt(string s) {
-
-        return ;
+        if (!s.length()) return 0;
+        int result = 0;
+        unordered_map<char, int> map;
+        map['i'] = 1;
+        map['I'] = 1;
+        map['v'] = 5;
+        map['V'] = 5;
+        map['x'] = 10;
+        map['X'] = 10;
+        map['l'] = 50;
+        map['L'] = 50;
+        map['c'] = 100;
+        map['C'] = 100;
+        map['d'] = 500;
+        map['D'] = 500;
+        map['m'] = 1000;
+        map['M'] = 1000;
+        for (int i = s.length() - 1; i >= 0; --i) {
+            if (!i) return map[s[i]];
+            result = map[s[i]] < map[s[i - 1]] ? (result - map[s[i]]): (result + map[s[i]]);
+        }
+        return result;
     }
 };
