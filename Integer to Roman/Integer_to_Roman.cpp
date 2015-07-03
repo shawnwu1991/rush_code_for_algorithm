70a1894ec12e6c8da6da3c335350c1cf6ead0811
Shawn Wu <shawnwu1991@gmail.com>
2015-07-03 23:44:32 +0800

    Algorithm(Medium) Integer to Roman.

--- Integer to Roman/Integer_to_Roman.cpp
+++ Integer to Roman/Integer_to_Roman.cpp
@@ -6,6 +6,11 @@ class Solution {
 public:
     string intToRoman(int num) {
         string result;
+        while (num >= 1) {
+            if (num / 1000 >= 1) {result.push_back('M'); num -= 1000; continue; }
+            if (num /  500 >= 1) {result.push_back('D'); num -=  500; continue; }
+
+        }
         return ;
     }
 };
