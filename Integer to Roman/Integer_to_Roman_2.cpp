d194b7f6b49e0112f34c943392e43bdf9c704df0
Shawn Wu <shawnwu1991@gmail.com>
2015-07-04 22:54:07 +0800

    Algorithm (Medium) Integer to Roman (Better Answer!).

--- Integer to Roman/Integer_to_Roman_2.cpp
+++ Integer to Roman/Integer_to_Roman_2.cpp
@@ -15,9 +15,9 @@ public:
         if  (n >=  50    ) {s +=  "L"; n -=   50; }
         if  (n >=  40    ) {s += "XL"; n -=   40; }
         for (; n /   10; ) {s +=  "X"; n -=   10; }
-        if  (n ==   9    ) {s += "IX"; n -=    9; }
+        if  (n ==   9    ) {s += "IX"; return result; }
         if  (n >=   5    ) {s +=  "V"; n -=    5; }
-        if  (n ==   4    ) {s += "IV"; n -=    4; }
+        if  (n ==   4    ) {s += "IV"; return result; }
         for (; n /    1; ) {s +=  "I"; n -=    1; }
         return result;
     }
