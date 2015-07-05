36cff8993bf772eb2032a676e7a60e6d467d10ef
Shawn Wu <shawnwu1991@gmail.com>
2015-07-05 22:37:54 +0800

    Algorithm(Easy) Longest Common Prefix.

--- Longest Common Prefix/Longest_Common_Prefix.cpp
+++ Longest Common Prefix/Longest_Common_Prefix.cpp
@@ -5,7 +5,11 @@ using namespace std;
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        string result = strs[0];
+        vector<string>::iterator iter = strs.begin();
+        while (iter != str.end()) {
 
+        }
         return ;
     }
 };
