7116e614a706708e872728a42417e964e6b5ba67
Shawn Wu <shawnwu1991@gmail.com>
2015-07-06 11:32:41 +0800

    Algorithm(Easy) Longest Common Prefix.

--- Longest Common Prefix/Longest_Common_Prefix.cpp
+++ Longest Common Prefix/Longest_Common_Prefix.cpp
@@ -8,8 +8,14 @@ public:
         string result = strs[0];
         vector<string>::iterator iter = strs.begin();
         while (iter != str.end()) {
-
+            int len1 = result.length(), len2 = *iter.length(), i = 0;
+            while (!len1 && !len2) {
+                if (result[i] == *iter[i]) {len1--; len2--; continue; }
+                else break;
+            }
+            if (!result.length()) return "";
+            iter++;
         }
-        return ;
+        return result;
     }
 };
