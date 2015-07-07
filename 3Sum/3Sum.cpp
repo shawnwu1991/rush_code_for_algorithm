a87b26123c44d4323b9140cee46ee20a87b0a0b4
Shawn Wu <shawnwu1991@gmail.com>
2015-07-08 00:07:46 +0800

    Algorithm(Medium) 3Sum.

--- 3Sum/3Sum.cpp
+++ 3Sum/3Sum.cpp
@@ -4,8 +4,21 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> thereSum(vector<int>& nums) {
-
-        return ;
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> results(0);
+        if (nums.size() < 3) return results;
+        unordered_map<int, int> mapping(-1);
+        for (int i = 0; i < nums.size(); ++i) mapping[nums[i]] = i;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (mapping[0 - nums[i]] >= 0) {
+                 vector<int> oneResult, otherTwo;
+                 oneResult.push_back(nums[mapping[0 - nums[i]]]);
+                 otherTwo = otherTwoSum(nums, mapping[0 - nums[i]], mapping);
+                 sort(oneResult, otherTwo);
+                 mapping[0 - nums[i]] = -1;
+                 //the other two nums' indices also being reset to -1.
+            }
+        }
+        return results;
     }
 };
