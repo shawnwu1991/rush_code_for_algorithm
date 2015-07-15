2403f510782654467437f38ae47d9f27e948f9a5
Shawn Wu <shawnwu1991@gmail.com>
2015-07-15 20:28:17 +0800

    Algorithm(Hard) Merge Intervals.

---
+++ Merge Intervals/Merge_Intervals.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+ * Definition for an interval.
+ * struct Interval {
+ *      int start;
+ *      int end;
+ *      Interval() : start(0), end(0) {}
+ *      Interval(int s, int e) : start(s), end(e) {}
+ * };*/
+
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval>& intervals) {
+        return ;
+    }
+};
