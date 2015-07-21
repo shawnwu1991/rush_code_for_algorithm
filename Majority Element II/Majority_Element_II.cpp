da33976e9bee34b0ceca3c58654d4d13125156b9
ShawnWu <shawnwu1991@gmail.com>
2015-07-21 19:46:26 +0800

    Update Majority_Element_II.cpp

--- Majority Element II/Majority_Element_II.cpp
+++ Majority Element II/Majority_Element_II.cpp
@@ -5,6 +5,8 @@ using namespace std;
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        return ;
+        vector<int> result(0);
+        if (nums.size() <= 1) return nums;
+        return result;
     }
 };
