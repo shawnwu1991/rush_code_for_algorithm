2d4b3f1be89cf45511de6a7b4dfa46f5f28af6ea
Shawn Wu <shawnwu1991@gmail.com>
2015-07-26 17:28:48 +0800

    Algorithm(Medium) Partition List.

---
+++ Partition List/Partition_List.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *      int val;
+ *      ListNode *next;
+ *      ListNode(int x) : val(x), next(NULL) {}
+ * };*/
+
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        return ;
+    }
+};
