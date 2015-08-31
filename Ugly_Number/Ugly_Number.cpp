588f3c62adbb3bc16bcf14b0e2027d3bd012e95e
Shawn Wu <shawnwu1991@gmail.com>
2015-08-31 20:44:41 +0800

    Algorithm(Easy) Ugly Number.

--- Ugly_Number/Ugly_Number.cpp
+++ Ugly_Number/Ugly_Number.cpp
@@ -4,6 +4,14 @@ using namespace std;
 class Solution {
     public:
         bool isUgly(int num) {
-            return false;
+            if (1 == num) return true;
+            if (num < 1) return false;
+            while (num > 1) {
+                if (!(num % 2)) {num = num / 2; continue; }
+                else if (!(num % 3)) {num = num / 3; continue; }
+                else if (!(num % 5)) {num = num / 5; continue; }
+                else return false;
+            }
+            return true;
         }
 };
