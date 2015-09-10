2b7fec553d260eb951aae0ce0c71c7913c063c9b
Shawn Wu <shawnwu1991@gmail.com>
2015-09-11 01:19:18 +0800

    Algorithm(Easy) First Bad Version.

---
+++ First Bad Version/First_Bad_Version.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+bool isBadVersion(int version);
+
+class Solution {
+    public:
+        int firstBadVersion(int n) {
+            return ;
+        }
+};
