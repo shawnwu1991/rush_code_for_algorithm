e1abc2de85cb004f581a4b128b139994ac252303
Shawn Wu <shawnwu1991@gmail.com>
2015-09-12 09:44:59 +0800

    Algorithm(Easy) First Bad Version.

--- First Bad Version/First_Bad_Version.cpp
+++ First Bad Version/First_Bad_Version.cpp
@@ -7,6 +7,22 @@ bool isBadVersion(int version);
 class Solution {
     public:
         int firstBadVersion(int n) {
-            return ;
+            if (!isBadVersion(n)) return -1;
+
+            int max = n, min = 1;
+            n = (max + min) / 2;
+
+            while (0 != (max - min)) {
+                 if (isBadVersion(n))
+                 {
+                     max = n;
+                     n = (min + max) / 2;
+                 }
+                 else {
+                      min = n;
+                      n = (max + min) / 2;
+                 }
+            }
+            return max;
         }
 };
