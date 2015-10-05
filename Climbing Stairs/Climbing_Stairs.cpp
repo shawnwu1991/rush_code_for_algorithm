6649875409a4bb9186f77bd8ca699da4e8cb3a89
Shawn Wu <shawnwu1991@gmail.com>
2015-10-05 14:59:47 +0800

    Algorithm(Easy) Climbing Stairs.

--- Climbing Stairs/Climbing_Stairs.cpp
+++ Climbing Stairs/Climbing_Stairs.cpp
@@ -5,6 +5,8 @@ using namespace std;
 class Solution {
 public:
     int climbStairs(int n) {
-    	
+    	if (1 == n) return 1;
+        if (2 == n) return 2;
+        return climbStairs(n - 1) + climbStairs(n - 2);
     }
 };
