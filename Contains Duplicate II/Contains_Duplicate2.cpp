8410570b408c2443a6080eab4c4d00a39359e31a
Shawn Wu <shawnwu1991@gmail.com>
2015-10-11 14:34:09 +0800

    Algorithm(Easy) Contains Duplicate II(Better!).

--- Contains Duplicate II/Contains_Duplicate2.cpp
+++ Contains Duplicate II/Contains_Duplicate2.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-    	return ;
+        unordered_map <int, int> mapping;
+        int i = 0;
+        while (i < nums.size()) {
+            if (mapping[nums[i]] == mapping.end()) mapping[nums[i]] = i;
+            else if ((i - mapping[nums[i]]) <= k) return true;
+            else return false;
+        }
     }
 };
