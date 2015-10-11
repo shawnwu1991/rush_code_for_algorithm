6fa5c84d39e7567f0a3dcd651e1c3136082a90b2
Shawn Wu <shawnwu1991@gmail.com>
2015-10-11 14:08:58 +0800

    Algorithm(Easy) Compare Version Numbers.

--- Compare Version Numbers/Compare_Version_Numbers.cpp
+++ Compare Version Numbers/Compare_Version_Numbers.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-    	if ((!version1) && (!version2)) return 0;
-	if (!version1) version1 = "0.";
-	if (!version2) version2 = "0.";
-	if (string::npos == version1.find('.')) version1.push_back('.');
-	if (string::npos == version2.find('.')) version2.push_back('.');
+        int size1 = version1.size();
+        int size2 = version2.size();
 
-   	int result = stoi(version1.substr(0, version1.find('.'))) - 
-		     stoi(version2.substr(0, version2.find('.')));
-	if (result > 0) return -1;
-	else if (result < 0) return 1;
-	else return compareVersion(version1.substr(version1.find('.'), version1.end()), version2.substr(version2.find('.'), version2.end()));
+        long int sum1, sum2;
+
+        for (size_t i = 0, j = 0; i < size1 || j < size2; ++i, ++j) {
+            sum1 = 0;
+            while (i < size1 && version1[i] != '.') {
+                 sum1 = sum1 * 10 + (version1[i] - '0');
+                 ++i;
+            }
+
+            sum2 = 0;
+            while (j < size2 && version2[i] != '.') {
+                 sum2 = sum2 * 10 + (version2[i] - '0');
+                 ++j;
+            }
+
+            if (sum1 < sum2) return -1;
+            else if (sum1 > sum2) return 1;
+
     }
+    return 0;
 };
